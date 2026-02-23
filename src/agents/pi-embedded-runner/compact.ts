khalf-code/aibot

feat(compaction): add timeout, model override, and diagnostic logging
- Add timeoutMs and model fields to AgentCompactionConfig type and Zod schema
- Wrap session.compact() with configurable timeout (default 120s) using abortCompaction()
- Support compaction-specific model override via config (e.g. use Haiku for summarization)
- Add diagnostic log lines for compaction start/done/timeout/error events
- Add compact.test.ts with 5 tests covering timeout, model override, error handling
- Extend overflow-compaction tests with timeout-during-overflow scenario

src/agents/pi-embedded-runner/compact.ts
@@ -30,6 +30,7 @@ import { formatUserTime, resolveUserTimeFormat, resolveUserTimezone } from "../d
 import { DEFAULT_MODEL, DEFAULT_PROVIDER } from "../defaults.js";
 import { resolveOpenClawDocsPath } from "../docs-path.js";
 import { getApiKeyForModel, resolveModelAuthMode } from "../model-auth.js";
+import { parseModelRef } from "../model-selection.js";
 import { ensureOpenClawModelsJson } from "../models-config.js";
 import {
   ensureSessionHeader,
@@ -121,9 +122,23 @@ export async function compactEmbeddedPiSessionDirect(
   const modelId = (params.model ?? DEFAULT_MODEL).trim() || DEFAULT_MODEL;
   const agentDir = params.agentDir ?? resolveOpenClawAgentDir();
   await ensureOpenClawModelsJson(params.config, agentDir);
+
+  // Override with compaction-specific model if configured
+  const compactionModelRef = params.config?.agents?.defaults?.compaction?.model?.trim();
+  let effectiveProvider = provider;
+  let effectiveModelId = modelId;
+  if (compactionModelRef) {
+    const parsed = parseModelRef(compactionModelRef, provider);
+    if (parsed) {
+      effectiveProvider = parsed.provider;
+      effectiveModelId = parsed.model;
+      log.info(`compaction: using override model ${effectiveProvider}/${effectiveModelId}`);
+    }
+  }
+
   const { model, error, authStorage, modelRegistry } = resolveModel(
-    provider,
-    modelId,
+    effectiveProvider,
+    effectiveModelId,
     agentDir,
     params.config,
   );
@@ -131,7 +146,7 @@ export async function compactEmbeddedPiSessionDirect(
     return {
       ok: false,
       compacted: false,
-      reason: error ?? `Unknown model: ${provider}/${modelId}`,
+      reason: error ?? `Unknown model: ${effectiveProvider}/${effectiveModelId}`,
     };
   }
   try {
@@ -434,7 +449,42 @@ export async function compactEmbeddedPiSessionDirect(
         if (limited.length > 0) {
           session.agent.replaceMessages(limited);
         }
-        const result = await session.compact(params.customInstructions);
+        const compactionTimeoutMs =
+          params.config?.agents?.defaults?.compaction?.timeoutMs ?? 120_000;
+        const compactionStartMs = Date.now();
+        let compactionTimedOut = false;
+
+        const timeoutHandle = setTimeout(() => {
+          compactionTimedOut = true;
+          session.abortCompaction();
+        }, compactionTimeoutMs);
+        timeoutHandle.unref();
+
+        log.info(
+          `compaction: start sessionId=${params.sessionId} ` +
+            `model=${effectiveProvider}/${effectiveModelId} timeoutMs=${compactionTimeoutMs}`,
+        );
+
+        let result: Awaited<ReturnType<typeof session.compact>>;
+        try {
+          result = await session.compact(params.customInstructions);
+        } catch (err) {
+          clearTimeout(timeoutHandle);
+          if (compactionTimedOut) {
+            log.warn(
+              `compaction: timeout sessionId=${params.sessionId} ` +
+                `timeoutMs=${compactionTimeoutMs} elapsedMs=${Date.now() - compactionStartMs}`,
+            );
+            return {
+              ok: false,
+              compacted: false,
+              reason: `compaction_timeout (${compactionTimeoutMs}ms)`,
+            };
+          }
+          throw err;
+        }
+        clearTimeout(timeoutHandle);
+
         // Estimate tokens after compaction by summing token estimates for remaining messages
         let tokensAfter: number | undefined;
         try {
@@ -450,6 +500,14 @@ export async function compactEmbeddedPiSessionDirect(
           // If estimation fails, leave tokensAfter undefined
           tokensAfter = undefined;
         }
+
+        log.info(
+          `compaction: done sessionId=${params.sessionId} ` +
+            `model=${effectiveProvider}/${effectiveModelId} ` +
+            `tokensBefore=${result.tokensBefore} tokensAfter=${tokensAfter ?? "?"} ` +
+            `durationMs=${Date.now() - compactionStartMs}`,
+        );
+
         return {
           ok: true,
           compacted: true,
@@ -469,6 +527,10 @@ export async function compactEmbeddedPiSessionDirect(
       await sessionLock.release();
     }
   } catch (err) {
+    log.warn(
+      `compaction: error sessionId=${params.sessionId} ` +
+        `reason=${describeUnknownError(err).slice(0, 200)}`,
+    );
     return {
       ok: false,
       compacted: false,

thebtf/openclaw

src/agents/pi-embedded-runner/compact.ts
@@ -33,6 +33,7 @@ import { formatUserTime, resolveUserTimeFormat, resolveUserTimezone } from "../d
 import { DEFAULT_MODEL, DEFAULT_PROVIDER } from "../defaults.js";
 import { resolveOpenClawDocsPath } from "../docs-path.js";
 import { getApiKeyForModel, resolveModelAuthMode } from "../model-auth.js";
+import { parseModelRef } from "../model-selection.js";
 import { ensureOpenClawModelsJson } from "../models-config.js";
 import { resolveOwnerDisplaySetting } from "../owner-display.js";
 import {
@@ -274,14 +275,28 @@ export async function compactEmbeddedPiSessionDirect(
   };
   const agentDir = params.agentDir ?? resolveOpenClawAgentDir();
   await ensureOpenClawModelsJson(params.config, agentDir);
+
+  // Override with compaction-specific model if configured
+  const compactionModelRef = params.config?.agents?.defaults?.compaction?.model?.trim();
+  let effectiveProvider = provider;
+  let effectiveModelId = modelId;
+  if (compactionModelRef) {
+    const parsed = parseModelRef(compactionModelRef, provider);
+    if (parsed) {
+      effectiveProvider = parsed.provider;
+      effectiveModelId = parsed.model;
+      log.info(`compaction: using override model ${effectiveProvider}/${effectiveModelId}`);
+    }
+  }
+
   const { model, error, authStorage, modelRegistry } = resolveModel(
-    provider,
-    modelId,
+    effectiveProvider,
+    effectiveModelId,
     agentDir,
     params.config,
   );
   if (!model) {
-    const reason = error ?? `Unknown model: ${provider}/${modelId}`;
+    const reason = error ?? `Unknown model: ${effectiveProvider}/${effectiveModelId}`;
     return fail(reason);
   }
   try {
@@ -650,7 +665,7 @@ export async function compactEmbeddedPiSessionDirect(
         if (diagEnabled && preMetrics) {
           log.debug(
             `[compaction-diag] start runId=${runId} sessionKey=${params.sessionKey ?? params.sessionId} ` +
-              `diagId=${diagId} trigger=${trigger} provider=${provider}/${modelId} ` +
+              `diagId=${diagId} trigger=${trigger} provider=${effectiveProvider}/${effectiveModelId} ` +
               `attempt=${attempt} maxAttempts=${maxAttempts} ` +
               `pre.messages=${preMetrics.messages} pre.historyTextChars=${preMetrics.historyTextChars} ` +
               `pre.toolResultChars=${preMetrics.toolResultChars} pre.estTokens=${preMetrics.estTokens ?? "unknown"}`,
@@ -660,9 +675,16 @@ export async function compactEmbeddedPiSessionDirect(
           );
         }
 
+        const compactionTimeoutMs =
+          params.config?.agents?.defaults?.compaction?.timeoutMs ?? undefined;
         const compactStartedAt = Date.now();
-        const result = await compactWithSafetyTimeout(() =>
-          session.compact(params.customInstructions),
+        log.info(
+          `compaction: start sessionId=${params.sessionId} ` +
+            `model=${effectiveProvider}/${effectiveModelId} timeoutMs=${compactionTimeoutMs ?? "default"}`,
+        );
+        const result = await compactWithSafetyTimeout(
+          () => session.compact(params.customInstructions),
+          compactionTimeoutMs,
         );
         // Estimate tokens after compaction by summing token estimates for remaining messages
         let tokensAfter: number | undefined;
@@ -698,11 +720,18 @@ export async function compactEmbeddedPiSessionDirect(
             });
         }
 
+        log.info(
+          `compaction: done sessionId=${params.sessionId} ` +
+            `model=${effectiveProvider}/${effectiveModelId} ` +
+            `tokensBefore=${result.tokensBefore} tokensAfter=${tokensAfter ?? "?"} ` +
+            `durationMs=${Date.now() - compactStartedAt}`,
+        );
+
         const postMetrics = diagEnabled ? summarizeCompactionMessages(session.messages) : undefined;
         if (diagEnabled && preMetrics && postMetrics) {
           log.debug(
             `[compaction-diag] end runId=${runId} sessionKey=${params.sessionKey ?? params.sessionId} ` +
-              `diagId=${diagId} trigger=${trigger} provider=${provider}/${modelId} ` +
+              `diagId=${diagId} trigger=${trigger} provider=${effectiveProvider}/${effectiveModelId} ` +
               `attempt=${attempt} maxAttempts=${maxAttempts} outcome=compacted reason=none ` +
               `durationMs=${Date.now() - compactStartedAt} retrying=false ` +
               `post.messages=${postMetrics.messages} post.historyTextChars=${postMetrics.historyTextChars} ` +